3b662014354d818ef0493c81fc1173e6853d2548
iqbalhossen0483 <rayhan.senbag.bd@gmail.com>
2022-03-30 14:02:27 +0600

    all done

--- src/component/Weather.tsx
+++ src/component/Weather.tsx
@@ -2,21 +2,25 @@ import { FC } from "react";
 
 const Weather: FC<{ weather: Weather }> = ({ weather }) => {
   return (
-    <div className='weather-report'>
-      <h2>Wather Report Of {weather.location.name}</h2>
-      <img src={weather.current.weather_icons[0]} alt='' />
-      <div className='space-y-2'>
-        <p>
-          <span>temperature: </span> {weather.current.temperature}
-        </p>
-        <p>
-          <span>wind_speed: </span> {weather.current.wind_speed}
-        </p>
-        <p>
-          <span>precip: </span> {weather.current.precip}
-        </p>
+    <>
+      <h2 className='weather-report-header'>
+        Weather Report Of {weather.location.name}
+      </h2>
+      <div className='weather-report'>
+        <img src={weather.current.weather_icons[0]} alt='' />
+        <div className='space-y-2 ml-5'>
+          <p>
+            <span>temperature: </span> {weather.current.temperature}
+          </p>
+          <p>
+            <span>wind_speed: </span> {weather.current.wind_speed}
+          </p>
+          <p>
+            <span>precip: </span> {weather.current.precip}
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
